392407e1e627d907c39184377bfcbaa636694ddb
Emiliano Acosta <75867405+EmilianoAcosta69@users.noreply.github.com>
2024-03-14 23:32:17 -0300

    se agrega funcionalidad a carrito

--- cart.js
+++ cart.js
@@ -5,6 +5,11 @@ const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
 
+    modalContainer.innerHTML = "";
+
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
+    //modal header
     const modalHeader = document.createElement("div");
     const modalClose = document.createElement("div");
     const modalContainer = document.getElementById("modal-container");
@@ -14,12 +19,18 @@ const displayCart = () => {
     modalClose.className = "modal-close";
     modalHeader.append(modalClose);
 
+    modalClose.addEventListener("click" , ()=> {
+        modalContainer.style.display = "none";
+        modalOverlay.style.display = "none";
+    })
+    
     const modalTitle = document.createElement("div");
     modalTitle.innerText = "carrito";
     modalTitle.className = "modal-title";
     modalHeader.append(modalTitle);
 
     modalContainer.append(modalHeader);
+    
 };
 
 cartBtn.addEventListener("click" , displayCart);
